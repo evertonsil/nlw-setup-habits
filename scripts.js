1c8db3a1425d037bd9945cbb0e259323fd923de1
evertonsil <everton.marques@uni9.edu.br>
2023-01-21 04:46:17 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('#form-habits');
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
+
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+//adiciona nova data
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert('Dia já incluso ❌');
+    return;
+  }
+  alert('Dia adicionado com sucesso ✅');
+  nlwSetup.addDay(today);
+}
+
+//salva o a opção do hábito em localStorage
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+// const data = {
+//   run: ['01-01', '01-02', '01-06'],
+//   water: ['01-04', '01-05'],
+//   food: ['01-01', '01-03'],
+//   journal: ['01-02'],
+//   takePills: ['01-03'],
+// };
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
